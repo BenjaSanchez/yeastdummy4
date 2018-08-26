ea1118f4ac2bfc185baa84280aaf07ff4ea86f31
benjasanchez <bensan@chalmers.se>
2018-08-26 12:39:14 +0200

    fix: metFormulas

--- scripts/test2.py
+++ scripts/test2.py
@@ -5,9 +5,9 @@ import cobra
 model = cobra.io.read_sbml_model('../model.xml')
 
 #Make changes:
-model.metabolites[1].formula = 'C3H2O7P'
-model.metabolites[2].formula = ''
-model.metabolites[3].formula = 'C6H10O10'
+model.metabolites[1].formula = 'C3H4O7P'
+model.metabolites[2].formula = 'C3H4O7P'
+model.metabolites[3].formula = 'C6H10O10P'
 
 #Save model:
 cobra.io.write_sbml_model(model,'../model.xml')
